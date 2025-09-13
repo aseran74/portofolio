c99f454561309011a7dca9c7b29bd6432d59ef0c
aseran74 <alvaroserr@gmail.com>
2025-09-13 19:33:23 +0200

    Fix: Mover estilos globales a index.html para evitar errores de TypeScript en Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,20 @@ import React from 'react'
 import { Footer } from './components/Footer/Footer'
 import { Header } from './components/Header/Header'
 import { Main } from './components/Main/Main'
-import { GlobalStyle } from './styles/global'
 import { Analytics } from "@vercel/analytics/react"
 import { LanguageProvider } from './contexts/LanguageContext'
 
 import 'react-toastify/dist/ReactToastify.css'
 
-// Type assertion to fix GlobalStyle component issue
-const GlobalStyleComponent = GlobalStyle as React.ComponentType
-
 function App() {
   return (
     <LanguageProvider>
-      <GlobalStyleComponent />
-      <Header></Header>
-      <Main></Main>
-      <Analytics />
-      <Footer></Footer>
+      <div>
+        <Header></Header>
+        <Main></Main>
+        <Analytics />
+        <Footer></Footer>
+      </div>
     </LanguageProvider>
   )
 }
